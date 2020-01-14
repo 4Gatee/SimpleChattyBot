eae88df9cf79c7580a19c67221e8ae3e480d7524
Vyacheslav <Stripslash@yandex.ru>
2020-01-14 16:07:49 +0300

    Update bot, now it ask your name and type greeting.

--- src/bot/SimpleBot.java
+++ src/bot/SimpleBot.java
@@ -1,8 +1,15 @@
 package bot;
+import java.util.Scanner;
 
 public class SimpleBot {
     public static void main(String[] args) {
         System.out.println("Hello! My name is Quantro.");
         System.out.println("I was created in 2020.");
+        System.out.println("Please, remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("What a great name your have, " + name + "!");
     }
 }
